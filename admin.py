7d68f00084b7a7caf76ade662d3f00ca87264976
senor-green-hill <juanabarro@protonmail.com>
2020-12-21 14:50:23 -0500

    url.py added. admin.py updated.

--- admin.py
+++ admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+class RequirementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rank', 'category')
+
+admin.site.register(Requirement, RequirementAdmin)
+admin.site.register(Category)
+admin.site.register(Rank)
+admin.site.register(Media)
+admin.site.register(Eligibility)
