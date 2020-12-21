40f486b5018030ef48ee8f3d847e7e4276168a05
senor-green-hill <juanabarro@protonmail.com>
2020-12-21 16:49:44 -0500

    added urls, views, and html

--- admin.py
+++ admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 # Register your models here.
 class RequirementAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rank', 'category')
+    list_display = ('name', 'category', 'rank')
 
 admin.site.register(Requirement, RequirementAdmin)
 admin.site.register(Category)
---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', RankListView.as_view(), name = 'rank_list_view'),
+    path('rank/<int:rank_id>/', RankDetailView.as_view(), name = 'rank_detail_view'),
+    path('rank/<int:rank_id>/category/<int:category_id>/requirement/<int:requirement_id>', RequirementDetailView.as_view(), name = 'requirement_detail_view'),
+    #path('category', CategoryDetailView.as_view(), name = 'category_detail_view'),
+]
